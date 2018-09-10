21933c9e14d8f6afebd1a105225b20f38ea4591f
Chance Hudson <jchancehud@gmail.com>
2018-09-09 23:57:10 -0500

    Add CSS loading support, fix missing imports/build

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ],
+      },
+      {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/,
